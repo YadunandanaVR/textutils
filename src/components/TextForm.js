dd6cfd3123cb3b1851a71d36986fa3c28be3cf0a
Yadunandana V R <yadunandanavr@gmail.com>
2021-08-23 17:41:25 +0530

    Add Dark/White mode Functionality

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -58,9 +58,34 @@ export default function TextForm(props) {
     setText(event.target.value);
   };
 
+  //dark mode / white mode and btn text(dark/white)
+  const [btnText, setBtnText] = useState("Enable Dark Mode");
+  const [myStyle, setMyStyle] = useState({
+    color: 'black',
+    backgroundColor: 'white'
+  });
+
+  const toggleMode = () => {
+    if (myStyle.color === 'black'){
+      setMyStyle({
+        color: 'white',
+        backgroundColor: 'black',
+        border: '2px solid white'
+      });
+      setBtnText("Enable LIght Mode");
+    }
+    else{
+      setMyStyle({
+        color: 'black',
+        backgroundColor: 'white'
+      });
+      setBtnText("Enable Dark Mode");
+    }
+  }
+
   return (
     <>
-      <div className="container my-3 ">
+      <div className="container my-3" style={myStyle}>
         <h1>{props.heading}</h1>
         <div className="mb-3">
           <textarea
@@ -69,17 +94,19 @@ export default function TextForm(props) {
             rows="8"
             value={text}
             onChange={HandleOnClick}
+            style={myStyle}
           ></textarea>
         </div>
-        <button className="btn btn-primary" onClick={HandleOnUpClick}>{" "}Convert to Upper</button>
-        <button className="btn btn-primary mx-2" onClick={HandleOnLowClick}>{" "}Convert to Lower</button>
-        <button className="btn btn-primary mx-2" onClick={HandleOnTitleClick}>{" "}Convert to Title{" "}</button>
-        <button className="btn btn-primary mx-2" onClick={HandleOnClearClick}>{" "}Clear Text{" "}</button>
-        <button className="btn btn-primary mx-2" onClick={HandleOnCopyClick}>{" "}Copy Text{" "}</button>
-        <button className="btn btn-primary mx-2" onClick={HandleOnDownloadClick}>{" "}Download{" "}</button>
+        <button className="btn btn-primary" onClick={HandleOnUpClick}>Convert to Upper</button>
+        <button className="btn btn-primary mx-2 my-3" onClick={HandleOnLowClick}>Convert to Lower</button>
+        <button className="btn btn-primary mx-2 my-3" onClick={HandleOnTitleClick}>Convert to Title</button>
+        <button className="btn btn-primary mx-2 my-3" onClick={HandleOnClearClick}>Clear Text</button>
+        <button className="btn btn-primary mx-2 my-3" onClick={HandleOnCopyClick}>Copy Text</button>
+        <button className="btn btn-primary mx-2 my-3" onClick={HandleOnDownloadClick}>Download</button>
+        <button className="btn btn-primary mx-2 my-3" onClick={toggleMode}>{btnText}</button>
       </div>
 
-      <div className="container my-3">
+      <div className="container my-3" style={myStyle}>
         <h2>Your text summary</h2>
         <p><b>{text.split(" ").length}</b> Words and <b>{text.length}</b>{" "} Characters</p>
         <p>{0.008 * text.split(" ").length.toFixed(4)} Minutes Read</p>
